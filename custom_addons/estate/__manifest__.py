c6b9c857511b4b424fd08590739351fa902d79c1
Salma Snawi <salmasnawi@gmail.com>
2026-08-21 00:38:48 +0300

    Initial estate module setup

---
+++ custom_addons/estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+    ],
+    'application': True,
+}
