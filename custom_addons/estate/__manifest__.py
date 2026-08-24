4b5043c6bb1ad3c195658f05a40b0d80e3059720
Salma Snawi <salmasnawi@gmail.com>
2026-08-25 01:25:20 +0300

    Complete estate module inheritance and reference code implementation

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -1,14 +1,16 @@
 {
     "name": "Real Estate",
     "version": "1.0",
-    "category": "Sales/CRM",
     "depends": [
         "base",
     ],
-    "data": [
-        "security/ir.model.access.csv",
-        "views/estate_property_views.xml",
-    ],
+"data": [
+    "security/ir.model.access.csv",
+    "views/estate_property_views.xml",
+    
+
+    "views/estate_property_inherit_views.xml",
+],
     "installable": True,
     "application": True,
 }
---
+++ custom_addons/estate/models/estate_property_inherit.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api
+
+
+class EstateProperty(models.Model):
+    _inherit = "estate.property"
+
+    reference_code = fields.Char(
+        string="Reference Code"
+    )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if not vals.get("reference_code"):
+                vals["reference_code"] = "PROP-AUTO"
+
+        return super().create(vals_list)
