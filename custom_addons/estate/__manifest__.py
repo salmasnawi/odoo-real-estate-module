5b8ff20679fb639bd5b1dd7ebd4b635aaa10d354
Salma Snawi <salmasnawi@gmail.com>
2026-08-25 15:37:48 +0300

    Complete Chapter 14: add QWeb property PDF report

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -4,15 +4,13 @@
     "depends": [
         "base",
     ],
-"data": [
-    "security/ir.model.access.csv",
+    "data": [
+        "security/ir.model.access.csv",
         "data/estate_sequence.xml",
-
-    "views/estate_property_views.xml",
-
-
-    "views/estate_property_inherit_views.xml",
-],
+        "views/estate_property_views.xml",
+        "report/estate_property_report.xml",
+        "views/estate_property_inherit_views.xml",
+    ],
     "installable": True,
     "application": True,
 }
